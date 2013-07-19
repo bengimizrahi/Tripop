0c1596b43963604b0ada58d14cf17036099df5c0
Bengi Mizrahi <bengi.mizrahi@argela.com.tr>
2013-07-19 16:50:55 +0300

    version 4

--- Ball.py
+++ Ball.py
@@ -1,6 +1,7 @@
 from common import *
 from cocos.euclid import *
 from cocos.sprite import Sprite
+from cocos.text import Label
 
 def createBall(approachingAngle, speed, type):
    x = GAME_AREA_RADIUS*math.cos(approachingAngle)
@@ -8,80 +9,104 @@ def createBall(approachingAngle, speed, type):
    newBall = Ball(x, y, speed, type)
    return newBall
 
-class Ball:
+class Ball(object):
+
+    idGenerator = IdGenerator()
+
     def __init__(self, x, y, speed, type):
-	self.sprite = Sprite(imageForBallType(type))
-	self.setPosition(x, y)
-	self.type = type
-	self.velocity = Vector2(-x, -y).normalize()*speed
-	self.hexagrid = None
+        self.id = self.idGenerator.next()
+        self.sprite = Sprite(imageForBallType(type))
+        self.sprite.position = x, y
+        self.type = type
+        self.velocity = Vector2(-x, -y).normalize()*speed
+        self.hexagrid = None
+        self.relCoordSystem = None
+        #l = Label(str(self.id), (-5, -5))
+        #self.sprite.add(l)
+        
     def __repr__(self):
-	return "[B:%.2f,%.2f,%s]" % (self.x, self.y, self.velocity)
-    def setPosition(self, x, y):
-	assert hasattr(self, 'sprite')
-	self.x, self.y = x, y
-	self.sprite.position = x, y
+        return "<Ball-%d>" % (self.id)
+
+    @property
     def position(self):
-	return self.x, self.y
+        if self.relCoordSystem:
+            x, y = self.sprite.position
+            angle = -1 * self.relCoordSystem.rotation
+            nx, ny = math.cos(angle)*x - math.sin(angle)*y, math.sin(angle)*x + math.cos(angle)*y
+            return nx, ny
+        else:
+            return self.sprite.position
+
+    @position.setter
+    def position(self, pos):
+        if self.relCoordSystem:
+            x, y = pos
+            angle = self.relCoordSystem.rotation
+            nx, ny = math.cos(angle)*x - math.sin(angle)*y, math.sin(angle)*x + math.cos(angle)*y
+            self.sprite.position = nx, ny
+        else:
+            self.sprite.position = pos
+
     def distance(self, ball):
-	x1, y1 = self.position()
-	x2, y2 = ball.position()
-	return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+        x1, y1 = self.position
+        x2, y2 = ball.position
+        return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
     def angleTo(self, ball):
-	x1, y1 = self.position()
-	x2, y2 = ball.position()
-	at = math.atan((y2-y1)/(x2-x1))
-	if x2 >= x1 and y2 >= y1:
-	    return at
-	elif x2 < x1 and y2 >= y1:
-	    return at + math.pi
-	elif x2 < x1 and y2 < y1:
-	    return at + math.pi
-	elif x2 >= x1 and y2 < y1:
-	    return at + 2*math.pi
+        x1, y1 = self.position
+        x2, y2 = ball.position
+        at = math.atan((y2-y1)/(x2-x1))
+        if x2 >= x1 and y2 >= y1:
+            return at
+        elif x2 < x1 and y2 >= y1:
+            return at + math.pi
+        elif x2 < x1 and y2 < y1:
+            return at + math.pi
+        elif x2 >= x1 and y2 < y1:
+            return at + 2*math.pi
+
     def move(self, dt):
-	x, y = self.position()
-	self.setPosition(x+(self.velocity.x*dt), y+(self.velocity.y*dt))
-    def conenct(self):
-	assert True, "NOT IMPLEMENTED"
+        x, y = self.position
+        self.position = x+(self.velocity.x*dt), y+(self.velocity.y*dt)
+
     def connect(self, ball):
-	angle = self.angleTo(ball)
-	x1, y1 = self.position()
-	x2, y2 = ball.position()
-	x = x2-x1
-	y = y2-y1
-	if x > 0 and y >= 0 and 0 <= angle < math.pi/3:
-	    nb_idx = 3
-	    x2 = 2*BALL_RADIUS*math.cos(math.pi/6)+x1
-	    y2 = 2*BALL_RADIUS*math.sin(math.pi/6)+y1
-	elif y >= 0 and math.pi/3 <= angle < 2*math.pi/3:
-	    nb_idx = 4
-	    x2 = x1
-	    y2 = 2*BALL_RADIUS+y1
-	elif x < 0 and y >= 0 and 2*math.pi/3 <= angle < math.pi:
-	    nb_idx = 5
-	    x2 = 2*BALL_RADIUS*math.cos(5*math.pi/6)+x1
-	    y2 = 2*BALL_RADIUS*math.sin(5*math.pi/6)+y1
-	elif x < 0 and y < 0 and math.pi <= angle < 4*math.pi/3:
-	    nb_idx = 0
-	    x2 = 2*BALL_RADIUS*math.cos(7*math.pi/6)+x1
-	    y2 = 2*BALL_RADIUS*math.sin(7*math.pi/6)+y1
-	elif y < 0 and 4*math.pi/3 <= angle < 5*math.pi/3:
-	    nb_idx = 1
-	    x2 = x1
-	    y2 = -2*BALL_RADIUS+y1
-	elif y < 0 and 5*math.pi/3 <= angle < 2*math.pi:
-	    nb_idx = 2
-	    x2 = 2*BALL_RADIUS*math.cos(11*math.pi/6)+x1
-	    y2 = 2*BALL_RADIUS*math.sin(11*math.pi/6)+y1
-	else:
-	    assert True, "connect error"
-	ball.setPosition(x2, y2)
-	nb_hexagrid = self.hexagrid.neighbours[nb_idx]
-	if nb_hexagrid == None:
-	    print "GAME OVER :))))"
-	    return
-	nb_hexagrid.ball = ball
-	ball.hexagrid = nb_hexagrid
-	ball.velocity *= 0
-	#ball.connect()
+        angle = self.angleTo(ball)
+        x1, y1 = self.position
+        x2, y2 = ball.position
+        x = x2-x1
+        y = y2-y1
+        if x > 0 and y >= 0 and 0 <= angle < math.pi/3:
+            nb_idx = 0
+            x2 = 2*BALL_RADIUS*math.cos(math.pi/6)+self.sprite.x
+            y2 = 2*BALL_RADIUS*math.sin(math.pi/6)+self.sprite.y
+        elif y >= 0 and math.pi/3 <= angle < 2*math.pi/3:
+            nb_idx = 1
+            x2 = self.sprite.x
+            y2 = 2*BALL_RADIUS+self.sprite.y
+        elif x < 0 and y >= 0 and 2*math.pi/3 <= angle < math.pi:
+            nb_idx = 2
+            x2 = 2*BALL_RADIUS*math.cos(5*math.pi/6)+self.sprite.x
+            y2 = 2*BALL_RADIUS*math.sin(5*math.pi/6)+self.sprite.y
+        elif x < 0 and y < 0 and math.pi <= angle < 4*math.pi/3:
+            nb_idx = 3
+            x2 = 2*BALL_RADIUS*math.cos(7*math.pi/6)+self.sprite.x
+            y2 = 2*BALL_RADIUS*math.sin(7*math.pi/6)+self.sprite.y
+        elif y < 0 and 4*math.pi/3 <= angle < 5*math.pi/3:
+            nb_idx = 4
+            x2 = self.sprite.x
+            y2 = -2*BALL_RADIUS+self.sprite.y
+        elif y < 0 and 5*math.pi/3 <= angle < 2*math.pi:
+            nb_idx = 5
+            x2 = 2*BALL_RADIUS*math.cos(11*math.pi/6)+self.sprite.x
+            y2 = 2*BALL_RADIUS*math.sin(11*math.pi/6)+self.sprite.y
+        else:
+            assert True, "connect error"
+        ball.position = x2, y2
+        nb_hexagrid = self.hexagrid.neighbours[nb_idx]
+        if nb_hexagrid == None:
+            print "GAME OVER :))))"
+            return
+        nb_hexagrid.ball = ball
+        ball.hexagrid = nb_hexagrid
+        ball.velocity *= 0
+        ball.sprite.stop()
--- Tripop.py
+++ Tripop.py
@@ -12,65 +12,95 @@ from cocos.euclid import *
 import pyglet
 from pyglet.window import key
 
+import pdb
 import math
 import random
 
 class CreateBallLogic:
+
     def __init__(self, gameLayer):
-	self.gameLayer = gameLayer
-	self.createBallInterval = 4.0
-	self.createBallTimer = self.createBallInterval
-	self.ballSpeed = 30 #px/sec
+        self.gameLayer = gameLayer
+        self.createBallInterval = 4.0
+        self.createBallTimer = self.createBallInterval
+        self.ballSpeed = 30 #px/sec
+
     def __call__(self, dt):
-	self.createBallTimer += dt
-	if (self.createBallTimer > self.createBallInterval):
-	    self.createBallInterval = max(1, self.createBallInterval-0.1)
-	    self.createBallTimer = 0.0
-	    angle = 2*math.pi*random.random()
-	    type = random.choice(range(1,5))
-	    newBall = createBall(angle, INITIAL_BALL_SPEED, type) 
-	    newBall.sprite.do(Repeat(RotateBy(360, 0.5)))
-	    self.gameLayer.addBall(newBall)
+        self.createBallTimer += dt
+        if (self.createBallTimer > self.createBallInterval):
+            self.createBallInterval = max(1, self.createBallInterval-0.1)
+            self.createBallTimer = 0.0
+            angle = 2*math.pi*random.random()
+            type = random.choice(range(1,5))
+            newBall = createBall(0, INITIAL_BALL_SPEED, type) 
+            newBall.relCoordSystem = gameLayer.hexameshLayer
+            #newBall.sprite.do(Repeat(RotateBy(360, 0.5)))
+            self.gameLayer.addBall(newBall)
+
+class HexameshLayer(Layer):
+
+    is_event_handler = True
+
+    def __init__(self):
+        super(HexameshLayer, self).__init__()
+        width, height = director.get_window_size()
+        self.children_anchor = width/2, height/2
+
+        self.hexamesh = Hexamesh(7, self)
+        self.centerGrid = self.hexamesh.center
+        self.centerGrid.setBall(Ball(0, 0, 0, BALL_TYPE_CORE))
+        self.add(self.centerGrid.ball.sprite)
+        self.attachedBalls = [self.centerGrid.ball]
+
+    def addBall(self, ball):
+        self.add(ball.sprite)
+        self.attachedBalls.append(ball)
+
+    def removeBall(self, ball):
+        self.remove(ball.sprite)
+        self.attachedBalls.remove(ball)
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        self.rotation = self.rotation + 180.0/GAME_AREA_RADIUS*dx
 
 class GameLayer(Layer):
-    is_event_handler = True
-    def __init__(self):
-#view
-	super(GameLayer, self).__init__()
-	width, height = director.get_window_size()
-	self.children_anchor = width/2, height/2
-#model
-	self.centerGrid = buildHexaMesh(7)
-	self.centerGrid.setBall(Ball(0, 0, 0, BALL_TYPE_CORE))
-	self.add(self.centerGrid.ball.sprite)
-	self.attachedBalls = [self.centerGrid.ball]
-	self.freeBalls = []
-	self.createBallLogic = CreateBallLogic(self)
-	self.mousePressed = False
-	self.schedule(self.step)
-    def step(self, dt):
-#start
-	justAttachedBalls = []
-	for aFreeBall in self.freeBalls:
-	    aFreeBall.move(dt)
-	    for anAttachedBall in self.attachedBalls:
-		if aFreeBall.distance(anAttachedBall) <= 2*BALL_RADIUS:
-		    anAttachedBall.connect(aFreeBall)
-		    justAttachedBalls.append(aFreeBall)
-		    self.attachedBalls.append(aFreeBall)
-		    break
-	for ball in justAttachedBalls:
-	    self.freeBalls.remove(ball)
-#logics
-	self.createBallLogic(dt)
-#end	
-    def addBall(self, ball):
-	self.add(ball.sprite)
-	self.freeBalls.append(ball)
-    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
-	self.rotation = self.rotation + 180/GAME_AREA_RADIUS*dx
 
+    def __init__(self):
+        super(GameLayer, self).__init__()
+        width, height = director.get_window_size()
+        self.children_anchor = width/2, height/2
+
+        self.freeBalls = []
+        self.createBallLogic = CreateBallLogic(self)
+        self.schedule(self.step)
+        self.hexameshLayer = HexameshLayer()
+
+    def step(self, dt):
+        justAttachedBalls = []
+        for aFreeBall in self.freeBalls:
+            aFreeBall.move(dt)
+            for anAttachedBall in self.hexameshLayer.attachedBalls:
+                if aFreeBall.distance(anAttachedBall) <= 2*BALL_RADIUS:
+                    anAttachedBall.connect(aFreeBall)
+                    justAttachedBalls.append(aFreeBall)
+                    self.removeBall(aFreeBall)
+                    self.hexameshLayer.addBall(aFreeBall)
+                    aFreeBall.relCoordSystem = None
+                    break
+        for ball in justAttachedBalls:
+            if ball in self.freeBalls: self.freeBalls.remove(ball)
+
+        self.createBallLogic(dt)
+
+    def addBall(self, ball):
+        self.add(ball.sprite)
+        self.freeBalls.append(ball)
+
+    def removeBall(self, ball):
+        self.remove(ball.sprite)
+        self.freeBalls.remove(ball)
+	
 if __name__ == "__main__":
     director.init(width=GAME_AREA_RADIUS*2, height=GAME_AREA_RADIUS*2, resizable=True)
-    director.run(Scene(GameLayer()))
+    gameLayer = GameLayer()
+    director.run(Scene(gameLayer, gameLayer.hexameshLayer))
 
